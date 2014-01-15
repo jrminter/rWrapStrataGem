0d96854e3e473db20c8a5ec0f081aca50410ab33
John Minter <jrminter@rochester.rr.com>
2014-01-15 00:05:51 -0500

    better inclusion of test data sets

---
+++ R/NiCu.R
@@ -0,0 +1,10 @@
+#' @name NiCu
+#' @title Ni/Cu//PET EDS K-Ratio Data
+#' @description This data contains the K-Ratios for stratified thin
+#' film analysis of Ni/Cu//PET.
+#' @docType data
+#' @usage NiCu
+#' @format a dataframe with 1 row per kV.
+#' @source Multi kV data set from FIB 620 with EDAX RTEM Detector
+#' @author John Minter, 2014-01-14
+NULL
---
+++ R/PdCu.R
@@ -0,0 +1,10 @@
+#' @name PdCu
+#' @title Pd/Cu//PET EDS K-Ratio Data
+#' @description This data contains the K-Ratios for stratified thin
+#' film analysis of Pd/Cu//PET.
+#' @docType data
+#' @usage CuPd
+#' @format a dataframe with 1 row per kV.
+#' @source Multi kV data set from FIB 620 with EDAX RTEM Detector
+#' @author John Minter, 2014-01-14
+NULL
